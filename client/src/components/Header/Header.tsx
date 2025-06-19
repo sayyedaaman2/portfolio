1be5a9d0b397af7bd40397333031ff97bdc49afc
Sayyed Aaman <sayyedaaman9@gmail.com>
2025-06-19 19:12:11 +0530

    Menu Hamburger Optimized

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,39 +1,45 @@
-'use client';
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 import Logo from "./Logo";
 import ThemeButton from "./ThemeButton";
 import HamburgerButton from "./HamburgerButton";
-import './hamburger-fix.css'
+import "./hamburger-fix.css";
 export default function Header() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
     <header className="h-24">
       <div className="px-4 lg:px-20 h-full flex justify-between items-center">
-        <Logo/>
+        <Logo />
         <div className="flex items-center lg:gap-5">
-          <nav id="navigation">
-            <ul className={`
-              fixed lg:static inset-0 z-40
+          <nav
+            id="navigation"
+            className={`fixed lg:static inset-0 z-40 lg:bg-transparent
+              transition-transform duration-300 ease-in-out
+              ${
+                isOpen ? "translate-x-0" : "-translate-x-full lg:translate-x-0"
+              }            backdrop-blur-md bg-white/30 dark:bg-black/30   p-6 shadow-lg`}
+          >
+            <ul
+              className={`
+              
               pt-24 lg:pt-0
               grid grid-cols-1 place-content-center lg:flex lg:gap-5
-              text-4xl lg:text-base uppercase
-              bg-slate-100 dark:bg-slate-800 lg:bg-transparent
-              transition-transform duration-300 ease-in-out
-              ${isOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}
-            `}>
-              <li className="nav-item">Home</li>
+              text-2xl lg:text-md lg:text-base uppercase
+             
+            `}
+            >
+              <li className="nav-item button">Home</li>
               <li className="nav-item">About</li>
               <li className="nav-item">Contact</li>
               <li className="nav-item">Project</li>
             </ul>
           </nav>
-          <ThemeButton/>
-          <HamburgerButton 
-          isActive={isOpen}
+          <ThemeButton />
+          <HamburgerButton
+            isActive={isOpen}
             onToggle={() => setIsOpen((prev) => !prev)}
             className="fill-red-500 lg:hidden"
-            
           />
         </div>
       </div>
